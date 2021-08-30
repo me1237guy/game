4c5b3628d09b9bf435b3c9fc76fbb18d61c6bd82
me1237guy <me1237guy@yahoo.com.tw>
2021-08-30 15:37:22 +0800

    change window caption

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pygame
 
+FPS = 60
+WHITE = (255, 255, 255)
+WIDTH = 500
+HEIGHT = 600
 pygame.init()
-screen = pygame.display.set_mode((500, 600))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("This is my first pygame.") 
+clock = pygame.time.Clock()
 
 running = True
 
 while running:
+    clock.tick(FPS)
     # (1) get all pygame events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -14,6 +21,7 @@ while running:
     # (2) update game
 
     # (3) display
-
+    screen.fill(WHITE)
+    pygame.display.update()
 
 pygame.quit()
