2481e078087d33052dfe5a315d3ae1acdb6cbcf8
me1237guy <me1237guy@yahoo.com.tw>
2021-08-30 14:27:15 +0800

    display a window

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((500, 600))
+
+running = True
+
+while running:
+    # (1) get all pygame events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # (2) update game
+
+    # (3) display
+
+
+pygame.quit()
